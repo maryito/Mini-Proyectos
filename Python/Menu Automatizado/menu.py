6c9d2a6218d7d7be1ca8c73d02f39b1f5762fc26
maryito <m_torres@uip.edu.pa>
2015-11-04 14:59:12 -0500

    Actualizando v.1.3

--- Python/Menu Automatizado/menu.py
+++ Python/Menu Automatizado/menu.py
@@ -1,18 +1,27 @@
 '''menu que recibira por parametro:
-un titulo y un apuntador a una tupla que contendra las opciones
+un titulo y la direccion de memoria de una tupla que contendra las opciones
+retornando la opcion elegida
+version 1.3
 '''
-
 def menu(principal,*opc):
-    
-    print(" MENU".center(45, "-"))
-    print("\n\t\t -%s- \n"%(principal.upper().title())) # titulo centrado
-    
-    for i in range (len(opc)):            
-        print("\t",i+1,"- ",str(opc[i]).capitalize()) # tabulado con primera letra en mayuscula
-    print("".center(45, "-"))
+    opciones = {}
+    validacion = False
+    while validacion!=True:
+        print(" MENU".center(45, "-"))
+        print("\n\t\t -%s- \n"%(principal.upper().title())) # titulo centrado
 
-'''
+        for i in range (len(opc)):
+            print("\t",i+1,"- ",str(opc[i]).capitalize()) # tabulado con primera letra en mayuscula
+            opciones[i+1]=opc[i]
+        print("".center(45, "-"))
+        eleccion = int(input("Esciba la opcion a Realizar --> "))
+        if  eleccion in opciones:
+            validacion= True
+        else:
+            print("Opcion incorrecta ")
+            validacion=False
+    return eleccion
+
 #Ejemplo
-men=("uno","dos","tres","cuarto","cinco") 
-menu("principal",*men)
-''''
+men=("uno","dos","tres","cuarto","cinco")
+print("su eleccion fue: "+str(menu("principal",*men)))
